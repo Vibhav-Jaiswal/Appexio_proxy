8cc6c24ff0c569eb59ecf78ee22ba718f1fad335
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-05-04 09:23:12 +0530

    add routes and create pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Proxies from "./pages/Proxies";
+import Pricing from "./pages/Pricing";
+import Resource from "./pages/Resource";
+import Blog from "./pages/Blog";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/proxies" element={<Proxies />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/resource" element={<Resource />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
